5204e5a6c5c472356ffd92adbb9633170e3f305a
robot001_001 <191415857@qq.com>
2025-12-17 20:26:26 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -2,7 +2,7 @@ python main.py \
     --logging_dir bylw/log/ \
     --mode dev \
     --dataset_name 'ml-20m' \
-    --max_sequence_length 200 \
+    --max_seq_len 200 \
     --positional_sampling_ratio 1.0 \
     --train_batch_size 1 \
     --eval_batch_size 1 \
