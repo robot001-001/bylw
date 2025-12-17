c5b56345083a1592338a0235d1776125a2f3339e
robot001_001 <191415857@qq.com>
2025-12-17 22:01:46 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -9,4 +9,4 @@ CUDA_LAUNCH_BLOCKING=1 python main.py \
     --eval_batch_size 1 \
     --device 'cuda:0' \
     --num_epochs 3 \
-    --model_args '{"max_seq_len": 400, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 3952, "linear_dim": 64, "attention_dim": 64, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 2, "num_heads": 1, "max_output_len": 11, "linear_dropout_rate": 0.0, "attn_dropout_rate": 0.0, "main_tower_units": [128, 5], "concat_ua": false, "enable_relative_attention_bias": true}'
+    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 3952, "linear_dim": 64, "attention_dim": 64, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 2, "num_heads": 1, "max_output_len": 11, "linear_dropout_rate": 0.0, "attn_dropout_rate": 0.0, "main_tower_units": [128, 5], "concat_ua": false, "enable_relative_attention_bias": true}'
