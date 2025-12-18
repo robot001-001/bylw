3c839633fc888c72b1175af869c98556bc91fd7d
robot001_001 <191415857@qq.com>
2025-12-18 22:40:12 +0800

    dev

--- my_model/model/HSTU.py
+++ my_model/model/HSTU.py
@@ -580,7 +580,7 @@ class HSTU(nn.Module):
                     num_heads=self._num_heads,
                     relative_attention_bias_module=(
                         RelativeBucketedTimeAndPositionBasedBias(
-                            max_seq_len=self._max_seq_len+1,
+                            max_seq_len=self._max_seq_len,
                             num_buckets=128,
                             bucketization_fn=lambda x: (
                                 torch.log(torch.abs(x).clamp(min=1)) / 0.301
