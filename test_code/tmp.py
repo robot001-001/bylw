ba432bbeb14c4b2050fafbdf305971b46f8f0797
robot001_001 <191415857@qq.com>
2025-12-18 19:58:17 +0800

    dev

--- test_code/tmp.py
+++ test_code/tmp.py
@@ -1,14 +1,10 @@
-from generative_recommenders.research.data.reco_dataset import get_reco_dataset
-from generative_recommenders.research.trainer.data_loader import create_data_loader
-from generative_recommenders.research.modeling.sequential.features import (
-    movielens_seq_features_from_row,
-)
 from generative_recommenders.research.data.dataset import DatasetV2
+from generative_recommenders.research.data.dataset_v3 import DatasetV3
 
 
 def main():
     batch_size = 1
-    dataset = DatasetV2(
+    dataset = DatasetV3(
         ratings_file="tmp/ml-1m/sasrec_format.csv",
         padding_length=200 + 1,  # target
         ignore_last_n=1,
