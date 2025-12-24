fd03677f9b62dbdb4c533dc2b0d03ffce5911534
robot001_001 <191415857@qq.com>
2025-12-24 14:18:16 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -11,7 +11,7 @@ python main.py \
     --train_batch_size 128 \
     --eval_batch_size 128 \
     --device 'cuda:0' \
-    --learning_rate 1e-4 \
+    --learning_rate 1e-3 \
     --num_epochs 101 \
     --eval_interval 20 \
-    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.5, "num_ratings": 5, "linear_dim": 64, "attention_dim": 64, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 2, "num_heads": 1, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.3, "main_tower_units": [128, 5], "concat_ua": false, "enable_relative_attention_bias": true}'
+    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 5, "linear_dim": 64, "attention_dim": 64, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 5], "concat_ua": false, "enable_relative_attention_bias": true}'
