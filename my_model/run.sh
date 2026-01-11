5863e65163645dd8f1b71964b3c35a8505d5627b
robot001_001 <191415857@qq.com>
2026-01-11 21:29:48 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -6,7 +6,7 @@ python main.py \
     --model HSTU \
     --mode dev \
     --dataset_name 'ml-1m' \
-    --use_binary_ratings False \
+    --use_binary_ratings True \
     --max_seq_len 200 \
     --embedding_dim 50 \
     --positional_sampling_ratio 1.0 \
@@ -17,4 +17,4 @@ python main.py \
     --learning_rate 1e-4 \
     --num_epochs 10 \
     --eval_interval 20 \
-    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.5, "num_ratings": 5, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 5], "concat_ua": false, "enable_relative_attention_bias": true}'
+    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.5, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 5], "concat_ua": false, "enable_relative_attention_bias": true}'
