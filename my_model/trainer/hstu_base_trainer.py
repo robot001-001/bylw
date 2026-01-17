4c99f15fad56c16e01d9fa0aea11aff374292d5b
robot001_001 <191415857@qq.com>
2026-01-17 15:19:42 +0800

    roll back

--- my_model/trainer/hstu_base_trainer.py
+++ my_model/trainer/hstu_base_trainer.py
@@ -322,7 +322,7 @@ class HSTUBaseTrainer:
                 )
                 
                 loss = self.criterion(outputs, (target_ratings-1).squeeze())
-                return
+                # return
                 
                 loss_to_display = loss.item()
                 loss = loss / self.accum_steps
