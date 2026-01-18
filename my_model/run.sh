6b2c8b7c6bec82923213f57b42e40a085db03447
robot001_001 <191415857@qq.com>
2026-01-18 18:39:36 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -32,7 +32,7 @@ python main.py \
     --embedding_dim 50 \
     --positional_sampling_ratio 1.0 \
     --train_batch_size 64 \
-    --accum_steps 1 \
+    --accum_steps 8 \
     --eval_batch_size 64 \
     --device 'cuda:0' \
     --learning_rate 1e-4 \
