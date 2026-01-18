c49d228d49e8c7864e7e54cac2c0950c683cf56d
robot001_001 <191415857@qq.com>
2026-01-18 17:50:29 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -9,7 +9,7 @@ python main.py \
     --use_binary_ratings True \
     --num_ratings 2 \
     --max_seq_len 200 \
-    --embedding_dim 50 \
+    --embedding_dim 128 \
     --positional_sampling_ratio 1.0 \
     --train_batch_size 512 \
     --accum_steps 1 \
@@ -18,7 +18,7 @@ python main.py \
     --learning_rate 1e-4 \
     --num_epochs 100 \
     --eval_interval 20 \
-    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+    --model_args '{"max_seq_len": 200, "embedding_dim": 128, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 64, "attention_dim": 64, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 # CUDA_LAUNCH_BLOCKING=1 \
 # python main.py \
