edb662ef2956b33537d0da8e9f4446e5e5c05cb7
robot001_001 <191415857@qq.com>
2026-02-06 00:01:31 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -134,7 +134,7 @@ clear
 # CUDA_LAUNCH_BLOCKING=1 \
 # python main.py \
 #     --logging_dir log/hstu_bsa_interleave_presort/ \
-#     --logging_file train_10.log \
+#     --logging_file train.log \
 #     --trainer_type HSTUBaseTrainer \
 #     --model HSTU_bsa_pretrain_interleave \
 #     --mode train_presort \
