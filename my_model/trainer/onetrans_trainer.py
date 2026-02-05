bd11f6850ceaa72b218d54c70a9dde93bb8068a7
robot001_001 <191415857@qq.com>
2026-02-05 23:32:51 +0800

    dev

--- my_model/trainer/onetrans_trainer.py
+++ my_model/trainer/onetrans_trainer.py
@@ -42,14 +42,34 @@ class ONETRANSTrainer:
 
     def define_flags(self):
         # setup params
-        flags.DEFINE_string('model', 'HSTU', 'HSTU/HSTU_nsa')
+        flags.DEFINE_string('model', 'ONETRANS', 'model file')
+        flags.DEFINE_string('mode', None, 'mode')
+        flags.DEFINE_string('device', 'cpu', 'device')
+        # shared params
+        flags.DEFINE_string('model_args', '{}', 'model args in json')
+        flags.DEFINE_string('dataset_name', 'ml-20m', 'dataset name')
+        flags.DEFINE_bool('use_binary_ratings', False, 'multi/binary ratings')
+        flags.DEFINE_integer('num_ratings', 5, 'num_ratings')
+        flags.DEFINE_integer('max_seq_len', 200, 'max seq len')
+        flags.DEFINE_integer('embedding_dim', 200, 'embedding_dim')
+        # train params
+        flags.DEFINE_string('train_data_dir', None, 'train_data_dir')
+        flags.DEFINE_integer('num_epochs', -1, 'num_epochs')
+        flags.DEFINE_integer('eval_interval', -1, 'eval_interval')
+        flags.DEFINE_integer('train_batch_size', -1, 'train_batch_size')
+        flags.DEFINE_float('learning_rate', 0, 'learning_rate')
+        flags.DEFINE_integer('accum_steps', 1, 'accum_steps')
+        # test params
+        flags.DEFINE_string('test_data_dir', None, 'test_data_dir')
+        flags.DEFINE_integer('eval_batch_size', -1, 'eval_batch_size')
         self.FLAGS = flags.FLAGS
 
 
     def run(self):
         logging.info(f'self.FLAGS.model: {self.FLAGS.model}')
-        if self.FLAGS.model == 'HSTU':
-            pass
+        if self.FLAGS.model == 'ONETRANS':
+            from model.ONETRANS import ONETRANS
+            self.model_cls = ONETRANS
         if self.FLAGS.mode == 'train':
             logging.info(f'mode: {self.FLAGS.mode}')
             self.train()
@@ -58,16 +78,18 @@ class ONETRANSTrainer:
         elif self.FLAGS.mode == 'dev':
             logging.info(f'mode: {self.FLAGS.mode}')
             self.dev()
-        elif self.FLAGS.mode == 'test_dev':
-            logging.info(f'mode: {self.FLAGS.mode}')
-            self.test_dev()
-        elif self.FLAGS.mode == 'train_presort':
-            logging.info(f'mode: {self.FLAGS.mode}')
-            self.train_presort()
 
     
     def get_model(self):
-        pass
+        self.model_args = json.loads(self.FLAGS.model_args)
+        logging.info(f'self.model_cls: {self.model_cls}')
+        self.model = self.model_cls(**self.model_args)
+        self.model.to(self.device)
+        total_params = sum(p.numel() for p in self.model.parameters())
+        trainable_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+
+        logging.info(f"Total Parameters: {total_params}")
+        logging.info(f"Trainable Parameters: {trainable_params}")
 
 
     def train(self):
