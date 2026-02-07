f8673049b803574a5887b9e92e58eca3fa9d1398
robot001_001 <191415857@qq.com>
2026-02-07 15:45:19 +0800

    dev

--- my_model/trainer/onetrans_trainer.py
+++ my_model/trainer/onetrans_trainer.py
@@ -117,7 +117,7 @@ class ONETRANSTrainer:
                 logging.info(f'batch: {batch_id}')
                 # logging.info(f'row: {row}')
                 input_embedding, tgt_ratings, s_len, ns_len = self.embedding_module(row)
-                logging.info(f'input_embedding.shape: {input_embedding.shape}')
+                # logging.info(f'input_embedding.shape: {input_embedding.shape}')
                 # logging.info(f'tgt_ratings: {tgt_ratings.shape}, {tgt_ratings}')
                 # logging.info(f's_len: {s_len.shape}, {s_len}')
                 ret = self.model(input_embedding, s_len)
@@ -132,6 +132,12 @@ class ONETRANSTrainer:
                     self.optimizer.zero_grad()
                 # logging.info(f'successfully train one batch')
                 # return
+
+            logging.info(f'start testing!')
+            avg_loss, avg_acc, avg_binary_acc, global_auc = self.test()
+            logging.info(f"[Eval] End of Epoch {epoch}: TrainLoss={loss_to_display:4g}, EvalLoss={avg_loss:.4f}, Acc={avg_acc:.4f}, BinaryAcc={avg_binary_acc:.4f}, AUC={global_auc:.4f}")
+            self.embedding_module.train()
+            self.model.train()
 
 
     def get_loss(self):
@@ -180,7 +186,48 @@ class ONETRANSTrainer:
 
 
     def test(self):
-        pass
+        self.embedding_module.eval()
+        self.model.eval()
+
+        batch_losses = []
+        batch_accs = []         # Exact Match Accuracy
+        batch_binary_accs = []  # Binary Accuracy
+
+        all_pos_probs = []
+        all_binary_targets = []
+
+        with torch.no_grad():
+            for row in iter(self.eval_data_loader):
+                input_embedding, tgt_ratings, s_len, ns_len = self.embedding_module(row)
+                outputs = self.model(input_embedding, s_len)
+                targets = (tgt_ratings - 1).view(-1)
+                loss = self.criterion(outputs, targets)
+                batch_losses.append(loss.item())
+
+                pred_ids = torch.argmax(outputs, dim=1)
+                acc = (pred_ids == targets).float().mean().item()
+                batch_accs.append(acc)
+
+                probs = torch.softmax(outputs, dim=1)
+                pos_probs_batch = probs[:, 1]
+                binary_targets_batch = targets.float()
+                binary_preds = (pos_probs_batch >= 0.5).float()
+                binary_acc = (binary_preds == binary_targets_batch).float().mean().item()
+                batch_binary_accs.append(binary_acc)
+                all_pos_probs.extend(pos_probs_batch.cpu().numpy().tolist())
+                all_binary_targets.extend(binary_targets_batch.cpu().numpy().tolist())
+        avg_loss = np.mean(batch_losses)
+        avg_acc = np.mean(batch_accs)
+        avg_binary_acc = np.mean(batch_binary_accs)
+
+        try:
+            global_auc = roc_auc_score(all_binary_targets, all_pos_probs)
+        except ValueError:
+            logging.warning("AUC calc failed: valid set needs both pos and neg samples.")
+            global_auc = 0.5
+
+        print(f"Debug: 预测为正例的比例: {binary_preds.mean().item():.2f}")
+        return avg_loss, avg_acc, avg_binary_acc, global_auc
 
 
     def test_dev(self):
