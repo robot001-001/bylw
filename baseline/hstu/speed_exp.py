a184355d85b453f316273d6f69e96e42a93970ba
robot001_001 <191415857@qq.com>
2026-02-11 21:56:57 +0800

    dev

--- baseline/hstu/speed_exp.py
+++ baseline/hstu/speed_exp.py
@@ -11,14 +11,15 @@ def generate_random_jagged_qkv(
     batch_size: int, 
     max_seq_len: int, 
     num_heads: int, 
-    dim: int
+    dim: int,
+    device
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, int]:
-    lengths = torch.randint(1, max_seq_len + 1, (batch_size,))
-    seq_offsets = torch.cat([torch.tensor([0]), torch.cumsum(lengths, dim=0)]).to(torch.int32)
+    lengths = torch.randint(1, max_seq_len + 1, (batch_size,), device=device)
+    seq_offsets = torch.cat([torch.tensor([0]), torch.cumsum(lengths, dim=0).to(device)]).to(torch.int32)
     total_L = seq_offsets[-1].item()
-    q = torch.randn(total_L, num_heads, dim)
-    k = torch.randn(total_L, num_heads, dim)
-    v = torch.randn(total_L, num_heads, dim)
+    q = torch.randn(total_L, num_heads, dim).to(device)
+    k = torch.randn(total_L, num_heads, dim).to(device)
+    v = torch.randn(total_L, num_heads, dim).to(device)
     return q, k, v, seq_offsets, max_seq_len
 
 
@@ -26,7 +27,7 @@ def speed_exp(
     Bsize, max_seq_len, num_heads, emb_dim
 ):
     ALPHA = 1.0 / (emb_dim ** 0.5)
-    q, k, v, seq_offsets, max_seq_len = generate_random_jagged_qkv(Bsize, max_seq_len, num_heads, emb_dim)
+    q, k, v, seq_offsets, max_seq_len = generate_random_jagged_qkv(Bsize, max_seq_len, num_heads, emb_dim, 'gpu:0')
     now = time.time()
     ret = hstu_mha(max_seq_len, ALPHA, q, k, v, seq_offsets, kernel="TRITON")
     time_cost = time.time()-now
