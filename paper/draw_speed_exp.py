192d6767430ec1de52a1ed57b0fe48a73d2d737b
robot001_001 <191415857@qq.com>
2026-02-12 21:16:58 +0800

    dev

--- paper/draw_speed_exp.py
+++ paper/draw_speed_exp.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 data_baseline = f'record/speed_exp_baseline.log'
 data_bsa = f'record/speed_exp_bsa.log'
+data_bsa_64 = f'record/speed_exp_bsa_64.log'
 
 def read_data(path):
     ret = {
@@ -24,11 +25,13 @@ def read_data(path):
 
 data_baseline = read_data(data_baseline)
 data_bsa = read_data(data_bsa)
+data_bsa_64 = read_data(data_bsa_64)
 
 figure, axs = plt.subplots(figsize = (10, 5), nrows=1, ncols=2)
 
 axs[0].plot(data_baseline['seq_len'], data_baseline['Time'], label='baseline')
-axs[0].plot(data_bsa['seq_len'], data_bsa['Time'], label='bsa')
+# axs[0].plot(data_bsa['seq_len'], data_bsa['Time'], label='bsa_32')
+axs[0].plot(data_bsa_64['seq_len'], data_bsa_64['Time'], label='bsa')
 
 axs[0].set_xlabel('seq lens')
 axs[0].set_ylabel('time cost(ms)')
@@ -36,11 +39,15 @@ axs[0].set_title('time cost stat')
 
 
 axs[1].plot(data_baseline['seq_len'], data_baseline['mem'], label='baseline')
-axs[1].plot(data_bsa['seq_len'], data_bsa['mem'], label='bsa')
+# axs[1].plot(data_bsa['seq_len'], data_bsa['mem'], label='bsa_32')
+axs[1].plot(data_bsa_64['seq_len'], data_bsa_64['mem'], label='bsa')
 
 axs[1].set_xlabel('seq lens')
 axs[1].set_ylabel('memory usage(MB)')
 axs[1].set_title('memory cost stat')
+
+
+
 
 plt.legend(title = 'label')
 plt.savefig('output/speed_exp.png')
