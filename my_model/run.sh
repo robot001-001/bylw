e0cc679fe14ee4ad871541146e0b7004e4c15828
robot001_001 <191415857@qq.com>
2026-02-15 13:34:54 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -89,33 +89,11 @@ clear
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
-python main.py \
-    --logging_dir log/exp_log/exp/ \
-    --logging_file exp_bsa_interleave_ml.log \
-    --trainer_type HSTUBaseTrainer \
-    --model HSTU_bsa_pretrain \
-    --mode dev \
-    --dataset_name 'ml-1m' \
-    --use_binary_ratings True \
-    --num_ratings 2 \
-    --max_seq_len 200 \
-    --embedding_dim 50 \
-    --positional_sampling_ratio 1.0 \
-    --train_batch_size 128 \
-    --accum_steps 4 \
-    --eval_batch_size 128 \
-    --device 'cuda:0' \
-    --learning_rate 3e-4 \
-    --num_epochs 100 \
-    --eval_interval 20 \
-    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
-
-
-# CUDA_LAUNCH_BLOCKING=1 \
 # python main.py \
-#     --logging_dir log/hstu_bsa_interleave/ \
+#     --logging_dir log/exp_log/ablation/ \
+#     --logging_file ablation_bsa_ml.log \
 #     --trainer_type HSTUBaseTrainer \
-#     --model HSTU_bsa_pretrain_interleave \
+#     --model HSTU_bsa_pretrain \
 #     --mode dev \
 #     --dataset_name 'ml-1m' \
 #     --use_binary_ratings True \
@@ -128,9 +106,32 @@ python main.py \
 #     --eval_batch_size 128 \
 #     --device 'cuda:0' \
 #     --learning_rate 3e-4 \
-#     --num_epochs 50 \
+#     --num_epochs 100 \
 #     --eval_interval 20 \
 #     --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+
+
+# CUDA_LAUNCH_BLOCKING=1 \
+python main.py \
+    --logging_dir log/exp_log/exp/ \
+    --logging_file exp_bsa_interleave_ml.log \
+    --trainer_type HSTUBaseTrainer \
+    --model HSTU_bsa_pretrain_interleave \
+    --mode dev \
+    --dataset_name 'ml-1m' \
+    --use_binary_ratings True \
+    --num_ratings 2 \
+    --max_seq_len 200 \
+    --embedding_dim 50 \
+    --positional_sampling_ratio 1.0 \
+    --train_batch_size 128 \
+    --accum_steps 4 \
+    --eval_batch_size 128 \
+    --device 'cuda:0' \
+    --learning_rate 3e-4 \
+    --num_epochs 50 \
+    --eval_interval 20 \
+    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 
 
