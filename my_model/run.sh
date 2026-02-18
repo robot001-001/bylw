e55aa6aeba3b0351842f11c8452cd6a390b0d58a
robot001_001 <191415857@qq.com>
2026-02-18 21:57:06 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -207,35 +207,11 @@ clear
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
-python main.py \
-    --logging_dir log/exp_log/baseline/ \
-    --logging_file baseline_hstu_am.log \
-    --trainer_type HSTUBaseTrainer \
-    --model HSTU_pretrain \
-    --mode dev \
-    --dataset_name 'amzn-books' \
-    --use_binary_ratings True \
-    --num_ratings 2 \
-    --max_seq_len 200 \
-    --embedding_dim 50 \
-    --positional_sampling_ratio 1.0 \
-    --train_batch_size 512 \
-    --accum_steps 1 \
-    --eval_batch_size 512 \
-    --device 'cuda:0' \
-    --learning_rate 3e-4 \
-    --num_epochs 100 \
-    --eval_interval 20 \
-    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
-
-
-
-# CUDA_LAUNCH_BLOCKING=1 \
 # python main.py \
-#     --logging_dir log/exp_log/ablation/ \
-#     --logging_file ablation_bsa_am.log \
+#     --logging_dir log/exp_log/baseline/ \
+#     --logging_file baseline_hstu_am.log \
 #     --trainer_type HSTUBaseTrainer \
-#     --model HSTU_bsa_pretrain \
+#     --model HSTU_pretrain \
 #     --mode dev \
 #     --dataset_name 'amzn-books' \
 #     --use_binary_ratings True \
@@ -243,14 +219,38 @@ python main.py \
 #     --max_seq_len 200 \
 #     --embedding_dim 50 \
 #     --positional_sampling_ratio 1.0 \
-#     --train_batch_size 128 \
-#     --accum_steps 4 \
-#     --eval_batch_size 128 \
+#     --train_batch_size 512 \
+#     --accum_steps 1 \
+#     --eval_batch_size 512 \
 #     --device 'cuda:0' \
 #     --learning_rate 3e-4 \
 #     --num_epochs 100 \
 #     --eval_interval 20 \
 #     --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+
+
+
+# CUDA_LAUNCH_BLOCKING=1 \
+python main.py \
+    --logging_dir log/exp_log/ablation/ \
+    --logging_file ablation_bsa_am.log \
+    --trainer_type HSTUBaseTrainer \
+    --model HSTU_bsa_pretrain \
+    --mode dev \
+    --dataset_name 'amzn-books' \
+    --use_binary_ratings True \
+    --num_ratings 2 \
+    --max_seq_len 200 \
+    --embedding_dim 50 \
+    --positional_sampling_ratio 1.0 \
+    --train_batch_size 128 \
+    --accum_steps 4 \
+    --eval_batch_size 128 \
+    --device 'cuda:0' \
+    --learning_rate 3e-4 \
+    --num_epochs 100 \
+    --eval_interval 20 \
+    --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
