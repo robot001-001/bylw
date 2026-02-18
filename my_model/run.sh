5bd5948df788715772ac3c8d8d225bd7ba9c0db2
robot001_001 <191415857@qq.com>
2026-02-18 23:11:08 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -247,7 +247,7 @@ python main.py \
     --accum_steps 4 \
     --eval_batch_size 128 \
     --device 'cuda:0' \
-    --learning_rate 1e-4 \
+    --learning_rate 3e-4 \
     --num_epochs 100 \
     --eval_interval 20 \
     --model_args '{"max_seq_len": 200, "embedding_dim": 50, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 25, "attention_dim": 25, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
