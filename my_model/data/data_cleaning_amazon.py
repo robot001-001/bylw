518f2984183caf21d135a554f3fa815439b8538f
robot001_001 <191415857@qq.com>
2026-02-18 16:31:17 +0800

    dev

--- my_model/data/data_cleaning_amazon.py
+++ my_model/data/data_cleaning_amazon.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 data = pd.read_csv('tmp/amzn_books/sasrec_format.csv')
 sequence_ratings = data['sequence_ratings'].to_list()
-sequence_ratings = [i.replace('1.0', '1').replace('2.0', '1').replace('3.0', '1').replace('4.0', '2').replace('5.0', '2') for i in sequence_ratings]
+sequence_ratings = [i.replace('1.0', '1').replace('2.0', '1').replace('3.0', '1').replace('4.0', '1').replace('5.0', '2') for i in sequence_ratings]
 data['sequence_ratings'] = sequence_ratings
 
 # print(data['sequence_ratings'].sample(1))
