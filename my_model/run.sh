8c8d913293783324355a18fed795aa076d633029
robot001_001 <191415857@qq.com>
2026-03-01 23:45:22 +0800

    dev

--- my_model/run.sh
+++ my_model/run.sh
@@ -23,26 +23,26 @@ clear
 #     --model_args '{"max_seq_len": 200, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 # CUDA_LAUNCH_BLOCKING=1 \
-python main.py \
-    --logging_dir log/exp_log/ablation/ \
-    --logging_file ablation_nsa_ml_v1.log \
-    --trainer_type HSTUBaseTrainer \
-    --model HSTU_nsa_pretrain \
-    --mode dev \
-    --dataset_name 'ml-1m' \
-    --use_binary_ratings True \
-    --num_ratings 2 \
-    --max_seq_len 200 \
-    --embedding_dim 64 \
-    --positional_sampling_ratio 1.0 \
-    --train_batch_size 64 \
-    --accum_steps 8 \
-    --eval_batch_size 64 \
-    --device 'cuda:0' \
-    --learning_rate 1e-4 \
-    --num_epochs 100 \
-    --eval_interval 20 \
-    --model_args '{"max_seq_len": 200, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+# python main.py \
+#     --logging_dir log/exp_log/ablation/ \
+#     --logging_file ablation_nsa_ml_v1.log \
+#     --trainer_type HSTUBaseTrainer \
+#     --model HSTU_nsa_pretrain \
+#     --mode dev \
+#     --dataset_name 'ml-1m' \
+#     --use_binary_ratings True \
+#     --num_ratings 2 \
+#     --max_seq_len 200 \
+#     --embedding_dim 64 \
+#     --positional_sampling_ratio 1.0 \
+#     --train_batch_size 64 \
+#     --accum_steps 8 \
+#     --eval_batch_size 64 \
+#     --device 'cuda:0' \
+#     --learning_rate 1e-4 \
+#     --num_epochs 100 \
+#     --eval_interval 20 \
+#     --model_args '{"max_seq_len": 200, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
@@ -69,26 +69,26 @@ python main.py \
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
-# python main.py \
-#     --logging_dir log/exp_log/exp/ \
-#     --logging_file exp_bsa_interleave_ml_v1.log \
-#     --trainer_type HSTUBaseTrainer \
-#     --model HSTU_bsa_pretrain_interleave \
-#     --mode dev \
-#     --dataset_name 'ml-1m' \
-#     --use_binary_ratings True \
-#     --num_ratings 2 \
-#     --max_seq_len 200 \
-#     --embedding_dim 64 \
-#     --positional_sampling_ratio 1.0 \
-#     --train_batch_size 128 \
-#     --accum_steps 4 \
-#     --eval_batch_size 128 \
-#     --device 'cuda:0' \
-#     --learning_rate 3e-4 \
-#     --num_epochs 100 \
-#     --eval_interval 20 \
-#     --model_args '{"max_seq_len": 200, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+python main.py \
+    --logging_dir log/exp_log/exp/ \
+    --logging_file exp_bsa_interleave_ml_v1.log \
+    --trainer_type HSTUBaseTrainer \
+    --model HSTU_bsa_pretrain_interleave \
+    --mode dev \
+    --dataset_name 'ml-1m' \
+    --use_binary_ratings True \
+    --num_ratings 2 \
+    --max_seq_len 200 \
+    --embedding_dim 64 \
+    --positional_sampling_ratio 1.0 \
+    --train_batch_size 128 \
+    --accum_steps 4 \
+    --eval_batch_size 128 \
+    --device 'cuda:0' \
+    --learning_rate 3e-4 \
+    --num_epochs 100 \
+    --eval_interval 20 \
+    --model_args '{"max_seq_len": 200, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 
 
