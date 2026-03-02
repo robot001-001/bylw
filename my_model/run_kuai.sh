b8f08e4ac5b72a44ac9044e35f8ff8315d735c4c
robot001_001 <191415857@qq.com>
2026-03-02 19:26:49 +0800

    dev

--- my_model/run_kuai.sh
+++ my_model/run_kuai.sh
@@ -46,34 +46,11 @@ clear
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
-python main.py \
-    --logging_dir log/exp_log/ablation/ \
-    --logging_file ablation_bsa_kuai_v1.log \
-    --trainer_type HSTUBaseTrainer \
-    --model HSTU_bsa_pretrain \
-    --mode dev \
-    --dataset_name 'kuai' \
-    --use_binary_ratings True \
-    --num_ratings 2 \
-    --max_seq_len 400 \
-    --embedding_dim 64 \
-    --positional_sampling_ratio 1.0 \
-    --train_batch_size 128 \
-    --accum_steps 4 \
-    --eval_batch_size 128 \
-    --device 'cuda:0' \
-    --learning_rate 3e-4 \
-    --num_epochs 100 \
-    --eval_interval 20 \
-    --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
-
-
-# CUDA_LAUNCH_BLOCKING=1 \
 # python main.py \
-#     --logging_dir log/exp_log/exp/ \
-#     --logging_file exp_bsa_interleave_kuai_v1.log \
+#     --logging_dir log/exp_log/ablation/ \
+#     --logging_file ablation_bsa_kuai_v1.log \
 #     --trainer_type HSTUBaseTrainer \
-#     --model HSTU_bsa_pretrain_interleave \
+#     --model HSTU_bsa_pretrain \
 #     --mode dev \
 #     --dataset_name 'kuai' \
 #     --use_binary_ratings True \
@@ -89,6 +66,29 @@ python main.py \
 #     --num_epochs 100 \
 #     --eval_interval 20 \
 #     --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+
+
+# CUDA_LAUNCH_BLOCKING=1 \
+python main.py \
+    --logging_dir log/exp_log/exp/ \
+    --logging_file exp_bsa_interleave_kuai_v1.log \
+    --trainer_type HSTUBaseTrainer \
+    --model HSTU_bsa_pretrain_interleave \
+    --mode dev \
+    --dataset_name 'kuai' \
+    --use_binary_ratings True \
+    --num_ratings 2 \
+    --max_seq_len 400 \
+    --embedding_dim 64 \
+    --positional_sampling_ratio 1.0 \
+    --train_batch_size 128 \
+    --accum_steps 4 \
+    --eval_batch_size 128 \
+    --device 'cuda:0' \
+    --learning_rate 3e-4 \
+    --num_epochs 100 \
+    --eval_interval 20 \
+    --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 
 
