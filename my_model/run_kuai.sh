220a0d232df03cc45528d446aa985e9c37521220
robot001_001 <191415857@qq.com>
2026-03-02 18:00:09 +0800

    dev

--- my_model/run_kuai.sh
+++ my_model/run_kuai.sh
@@ -23,26 +23,26 @@ clear
 #     --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 # CUDA_LAUNCH_BLOCKING=1 \
-python main.py \
-    --logging_dir log/exp_log/ablation/ \
-    --logging_file ablation_nsa_kuai_v1.log \
-    --trainer_type HSTUBaseTrainer \
-    --model HSTU_nsa_pretrain \
-    --mode dev \
-    --dataset_name 'kuai' \
-    --use_binary_ratings True \
-    --num_ratings 2 \
-    --max_seq_len 400 \
-    --embedding_dim 64 \
-    --positional_sampling_ratio 1.0 \
-    --train_batch_size 32 \
-    --accum_steps 16 \
-    --eval_batch_size 32 \
-    --device 'cuda:0' \
-    --learning_rate 1e-4 \
-    --num_epochs 100 \
-    --eval_interval 20 \
-    --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+# python main.py \
+#     --logging_dir log/exp_log/ablation/ \
+#     --logging_file ablation_nsa_kuai_v1.log \
+#     --trainer_type HSTUBaseTrainer \
+#     --model HSTU_nsa_pretrain \
+#     --mode dev \
+#     --dataset_name 'kuai' \
+#     --use_binary_ratings True \
+#     --num_ratings 2 \
+#     --max_seq_len 400 \
+#     --embedding_dim 64 \
+#     --positional_sampling_ratio 1.0 \
+#     --train_batch_size 32 \
+#     --accum_steps 16 \
+#     --eval_batch_size 32 \
+#     --device 'cuda:0' \
+#     --learning_rate 1e-4 \
+#     --num_epochs 100 \
+#     --eval_interval 20 \
+#     --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 # no need to run
 # CUDA_LAUNCH_BLOCKING=1 \
@@ -163,25 +163,25 @@ python main.py \
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
-# python main.py \
-#     --logging_dir log/exp_log/baseline/ \
-#     --logging_file baseline_onetrans_kuai_v1.log \
-#     --trainer_type ONETRANSTrainer \
-#     --model ONETRANS \
-#     --mode train \
-#     --dataset_name 'kuai' \
-#     --use_binary_ratings True \
-#     --num_ratings 2 \
-#     --max_seq_len 400 \
-#     --embedding_dim 64 \
-#     --train_batch_size 512 \
-#     --accum_steps 1 \
-#     --eval_batch_size 512 \
-#     --device 'cuda:0' \
-#     --learning_rate 3e-4 \
-#     --num_epochs 20 \
-#     --eval_interval 20 \
-#     --model_args '{"num_layers": 8, "max_seq_len": [1024, 512, 256, 128, 64, 32, 16, 8], "ns_seq_len": 2, "d_model": 64, "num_heads": 2, "ffn_layer_hidden_dim": 128, "main_tower_units": [128, 2]}'
+python main.py \
+    --logging_dir log/exp_log/baseline/ \
+    --logging_file baseline_onetrans_kuai_v1.log \
+    --trainer_type ONETRANSTrainer \
+    --model ONETRANS \
+    --mode train \
+    --dataset_name 'kuai' \
+    --use_binary_ratings True \
+    --num_ratings 2 \
+    --max_seq_len 400 \
+    --embedding_dim 64 \
+    --train_batch_size 512 \
+    --accum_steps 1 \
+    --eval_batch_size 512 \
+    --device 'cuda:0' \
+    --learning_rate 3e-4 \
+    --num_epochs 20 \
+    --eval_interval 20 \
+    --model_args '{"num_layers": 8, "max_seq_len": [1024, 512, 256, 128, 64, 32, 16, 8], "ns_seq_len": 2, "d_model": 64, "num_heads": 2, "ffn_layer_hidden_dim": 128, "main_tower_units": [128, 2]}'
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
