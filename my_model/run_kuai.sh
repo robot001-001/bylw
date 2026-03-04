6a1057b50fcc6115d7e2571f57dfd6635d13ebcf
robot001_001 <191415857@qq.com>
2026-03-04 22:37:23 +0800

    dev

--- my_model/run_kuai.sh
+++ my_model/run_kuai.sh
@@ -1,26 +1,26 @@
 git pull
 clear
 # CUDA_LAUNCH_BLOCKING=1 \
-# python main.py \
-#     --logging_dir log/exp_log/baseline/ \
-#     --logging_file baseline_hstu_kuai_v1.log \
-#     --trainer_type HSTUBaseTrainer \
-#     --model HSTU_pretrain \
-#     --mode dev \
-#     --dataset_name 'kuai' \
-#     --use_binary_ratings True \
-#     --num_ratings 2 \
-#     --max_seq_len 400 \
-#     --embedding_dim 64 \
-#     --positional_sampling_ratio 1.0 \
-#     --train_batch_size 128 \
-#     --accum_steps 4 \
-#     --eval_batch_size 128 \
-#     --device 'cuda:0' \
-#     --learning_rate 3e-4 \
-#     --num_epochs 100 \
-#     --eval_interval 20 \
-#     --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
+python main.py \
+    --logging_dir log/exp_log/baseline/ \
+    --logging_file baseline_hstu_kuai_v1.log \
+    --trainer_type HSTUBaseTrainer \
+    --model HSTU_pretrain \
+    --mode dev \
+    --dataset_name 'kuai' \
+    --use_binary_ratings True \
+    --num_ratings 2 \
+    --max_seq_len 400 \
+    --embedding_dim 64 \
+    --positional_sampling_ratio 1.0 \
+    --train_batch_size 128 \
+    --accum_steps 4 \
+    --eval_batch_size 128 \
+    --device 'cuda:0' \
+    --learning_rate 3e-4 \
+    --num_epochs 100 \
+    --eval_interval 20 \
+    --model_args '{"max_seq_len": 400, "embedding_dim": 64, "dropout_rate": 0.2, "num_ratings": 2, "linear_dim": 32, "attention_dim": 32, "normalization": "rel_bias", "linear_config": "uvqk", "linear_activation": "silu", "num_blocks": 8, "num_heads": 2, "linear_dropout_rate": 0.2, "attn_dropout_rate": 0.0, "main_tower_units": [128, 2], "concat_ua": false, "enable_relative_attention_bias": true}'
 
 # CUDA_LAUNCH_BLOCKING=1 \
 # python main.py \
@@ -140,25 +140,25 @@ clear
 
 
 # CUDA_LAUNCH_BLOCKING=1 \
-python main.py \
-    --logging_dir log/exp_log/baseline/ \
-    --logging_file baseline_rankmixer_kuai_v1.log \
-    --trainer_type RANKMIXERTrainer \
-    --model RANKMIXER \
-    --mode train \
-    --dataset_name 'kuai' \
-    --use_binary_ratings True \
-    --num_ratings 2 \
-    --max_seq_len 400 \
-    --embedding_dim 64 \
-    --train_batch_size 512 \
-    --accum_steps 1 \
-    --eval_batch_size 512 \
-    --device 'cuda:0' \
-    --learning_rate 3e-4 \
-    --num_epochs 20 \
-    --eval_interval 20 \
-    --model_args '{"topk": 20, "max_seq_len": 400, "d_model": 64, "sim_main_tower_units": [512, 384], "num_blocks": 2, "num_heads": 8, "main_tower_units": [128, 2]}'
+# python main.py \
+#     --logging_dir log/exp_log/baseline/ \
+#     --logging_file baseline_rankmixer_kuai_v1.log \
+#     --trainer_type RANKMIXERTrainer \
+#     --model RANKMIXER \
+#     --mode train \
+#     --dataset_name 'kuai' \
+#     --use_binary_ratings True \
+#     --num_ratings 2 \
+#     --max_seq_len 400 \
+#     --embedding_dim 64 \
+#     --train_batch_size 512 \
+#     --accum_steps 1 \
+#     --eval_batch_size 512 \
+#     --device 'cuda:0' \
+#     --learning_rate 3e-4 \
+#     --num_epochs 20 \
+#     --eval_interval 20 \
+#     --model_args '{"topk": 20, "max_seq_len": 400, "d_model": 64, "sim_main_tower_units": [512, 384], "num_blocks": 2, "num_heads": 8, "main_tower_units": [128, 2]}'
 
 
 
